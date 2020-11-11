c6a426706dd875fe78d5e652ab973e6db2809de1
Radwa Ahmed <52842936+radwaahmed20112000@users.noreply.github.com>
2020-11-11 22:18:12 +0200

    Start vue project

---
+++ calcu/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>hello, {{ user.firstname + user.secondname }}</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            followers: 0,
+            user: {
+                firstname: "Radwa",
+                secondname: "Ahmed",
+            }
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
